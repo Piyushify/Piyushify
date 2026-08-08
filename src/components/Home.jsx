b7aa97b69c8700491c0309f38ee6d9d1fdf7b6c4
Piyushify <piyushsharma3042007@gmail.com>
2026-08-08 14:06:23 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import dp from "../assets/dp_smile3.png"
+import dp from "../assets/dp.webp"
 import Githubicon from "./icons/Githubicon.jsx"
 import LinkedInicon from "./icons/LinkedInicon.jsx"
 import Emailicon from "./icons/Emailicon.jsx"
@@ -6,7 +6,7 @@ function Home() {
   let greet = "Hi there 👋🏼";
   let info = "A BCA student.";
   return (
-    <section className="h-screen flex items-center flex-col justify-center p-6 pt-20">
+    <section className="h-svh flex items-center flex-col justify-center p-6 pt-20">
       <img src={dp} className="w-32 h-32 self-start rounded-full shadow-md backdrop-blur-4xl bg-current/2"/>
       <p className="text-3xl md:text-5xl lg:text-7xl w-screen py-3 px-6 font-semibold">{greet}</p>
       <p className="text-3xl md:text-5xl lg:text-7xl">I'm <b className="underline decoration-[#bb8866aa] decoration-dashed decoration-2">Piyush Sharma</b>.</p>
--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,7 +1,7 @@
 function Navigator() {
   return (
-    <nav className="fixed right-0 -translate-y-[50%] top-[50dvh] flex backdrop-blur-sm items-center px-2 py-3 pr-3 rounded-l-3xl shadow-md duration-200 ease-in-out">
-      <div className="z-998 *:w-2.5 *:h-2.5 *:bg-current gap-0.5 *:rounded-sm  rounded-xl grid grid-cols-2 inset-shadow-sm shadow-sm border-2 border-current/2 backdrop-blur-sm p-1.25">
+    <nav className="fixed right-0 -translate-y-[50%] top-[50dvh] flex backdrop-blur-sm items-center px-2 py-3 pr-3 rounded-l-3xl shadow-md shadow-[#bb8866]/15 duration-200 ease-in-out">
+      <div className="z-998 *:w-2.5 *:h-2.5 *:bg-current gap-0.5 *:rounded-sm  rounded-xl grid grid-cols-2 inset-shadow-sm shadow-md shadow-[#bb8866]/15 border-2 border-current/2 backdrop-blur-sm p-1.25">
         <div></div>
         <div></div>
         <div></div>
